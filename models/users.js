fe0e585b3de7a368983f46210dcbed0e7130465e
Fabian Flores <ffflores1@outlook.com>
2018-08-02 21:32:49 -0400

    added users definition

---
+++ models/users.js
@@ -0,0 +1,92 @@
+// Requiring bcrypt for password hashing. Using the bcrypt-nodejs version as the regular bcrypt module
+// sometimes causes errors on Windows machines
+var bcrypt = require("bcrypt-nodejs");
+
+module.exports = function(sequelize, DataTypes) {
+
+    var Users = sequelize.define("Users", {
+        user_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true
+        },
+        first_name: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        last_name: {
+          type: DataTypes.STRING,
+          allowNull: false
+        },
+        email: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          unique: true,
+          validate: {isEmail: true}
+        },
+        user_pw: {
+          type: DataTypes.STRING,
+          allowNull: false,
+          validate: {
+            len: [7]
+          }
+        },
+        user_photo: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: "https://placem.at/things?w=250&h=250&random=some_seed",
+            validate: {
+                isUrl: true
+            }
+        },
+        user_type: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: "user"
+        }
+      },
+      {
+        freezeTableName: true,
+        underscored: true
+      });
+
+      // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+      Users.prototype.validPassword = function(user_pw) {
+        return bcrypt.compareSync(user_pw, this.user_pw);
+      };
+      // Hooks are automatic methods that run during various phases of the User Model lifecycle
+      // after a password is validated (on create or update), automatically hash password
+      Users.hook("afterValidate", function(user) {
+        user.user_pw = bcrypt.hashSync(user.user_pw, bcrypt.genSaltSync(10), null);
+      });
+
+    return Users;
+};
+
+/*
+
+,
+        // ============================================================================
+        // user_votes field may not be needed here, we'll leave it in for now
+        //
+        // ============================================================================
+        user_votes: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: 0
+        }
+
+
+      Users.associate = function(models) {
+        // topics have many choices
+        Users.belongsToMany(models.Topics, {
+          through: {
+            model: models.Choices,
+            unique: false
+          },
+          foreignKey: "user_id",
+          constraints: false
+        });
+          };
+
+*/
