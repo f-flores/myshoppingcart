8ef1553c9fa48c08725e3355cc598e0d90b17b35
Fabian Flores <ffflores1@outlook.com>
2018-08-13 21:43:22 -0400

    work on App.js and isAdmin.js middleware

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+
+// import VerticalNav from "./components/VerticalNav";
+// import Footer from "./components/Footer";
+// import EntryMessage from "./components/EntryMessage";
+// import Home from "./containers/Home";
+// import About from "./containers/About";
+// import Videos from "./containers/Videos";
+// import Chat from "./containers/Chat";
+// import Glossary from "./containers/Glossary";
+// import Admin from "./containers/Admin";
+// import Post from "./containers/Post";
+// import Login from "./containers/Login";
+// import Signup from "./containers/Signup";
+// import Logout from "./containers/Logout";
+// import AUTH from "./utilities/AUTH";
+// import ScrollToTop from "./components/ScrollToTop";
+
+
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      // --
+      // 'session' variables
+      // ---------------------------
+      isLoggedIn: false,
+      isAdmin: false,
+      isCustomer: false,
+      isEmployee: false,
+      isManager: false,
+      username: "",
+      email: "",
+      userId: "",
+      usertestvalue: "hello",
+      returnStatus: 0,
+      errorMsg: "",
+      redirectReferrer: false
+    };
+  }
+
   render() {
     return (
       <div className="App">
