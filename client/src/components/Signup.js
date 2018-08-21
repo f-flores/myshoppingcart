faed60b5ca178264be5306b2112d3b2254993781
Fabian Flores <ffflores1@outlook.com>
2018-08-21 12:08:19 -0400

    using formik and yup libraries for validation on signup

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -6,24 +6,25 @@
 
 import React, {Component} from "react";
 import {Redirect} from "react-router-dom";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
 // import AUTH from "../utilities/AUTH";
 import {ErrorUserName, ErrorPassword, ErrorEmail, ErrorPasswordMatch} from "./ErrorComponents";
 import {MinUsernameLength, MaxUsernameLength, MinPasswordLength} from "../constants/Consts";
 
 class Signup extends Component {
-  state = {
-    success: false,
-    isValidUsername: true,
-    isValidEmail: true,
-    isValidPassword: true,
-    doPasswordsMatch: true,
-    username: "",
-    password: "",
-    pswrdConfirmation: "",
-    email: "",
-    userId: 0,
-    errorMsg: ""
+  constructor({values, handleChange}) {
+    super();
+    this.state = {
+      success: false,
+      username: "",
+      password: "",
+      pswrdConfirmation: "",
+      email: "",
+      userId: 0,
+      errorMsg: ""
+    }
   }
 
   componentDidMount() {
@@ -44,101 +45,23 @@ class Signup extends Component {
     this.setState({[name]: value});
   }
 
-  displayErrorMessage() {
-    if (this.state.errorMsg !== "") {
-      return (
-        <span className="form-control bg-danger text-white mb-2">
-        {this.state.errorMsg}
-        </span>
-      );
-    }
-  }
-
   render() {
     return (
     <div className="container py-5">
     <div className="row justify-content-center text-center">
     
-      <form className="col-12 col-md-6 my-1">
         <h1 className="col-12">Sign Up</h1>
-        <div className="col-12 my-1">
-          <i class="fas fa-key"></i>
-        </div> 
-        <div className="form-group">
-          <input
-            type="text"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleInputChange}
-            className="form-control"
-            placeholder="Enter Username" />
-          
-        </div>
-        { !this.state.isValidUsername 
-          ? <ErrorUserName 
-              ErrorInUserName={!this.state.isValidUserName} 
-              UnameMinLength={MinUsernameLength}
-              UnameMaxLength={MaxUsernameLength}
-            />
-          : null
-        }
+        <Form className="col-12 col-md-6 my-1">
+          <Field type="text" name="username" className="form-control" placeholder="Enter Username" />
 
-        <div className="form-group">
-          <input
-            type="text"
-            name="email"
-            value={this.state.email}
-            onChange={this.handleInputChange}
-            className="form-control"
-            placeholder="Enter Email" />
+          <Field type="email" name="email" className="form-control" placeholder="Enter Email" />
           
-        </div>
-        { !this.state.isValidEmail 
-          ? <ErrorEmail ErrorInEmail={!this.state.isValidEmail} />
-          : null
-        }
+          <Field type="password" name="password" className="form-control" placeholder="Enter Password"/>
 
-        <div className="form-group">
-          <input
-            type="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleInputChange}
-            className="form-control"
-            placeholder="Enter Password"
-          />
-        </div>
-        { !this.state.isValidPassword 
-          ? <ErrorPassword 
-              ErrorInPassword={!this.state.isValidPassword} 
-              MinPasswordLength={MinPasswordLength}
-            />
-          : null
-        }
-        <div className="form-group">
-          <input
-            type="password"
-            name="pswrdConfirmation"
-            value={this.state.pswrdConfirmation}
-            onChange={this.handleInputChange}
-            className="form-control"
-            placeholder="Confirm Password"
-          />
-        </div>
-        { !this.state.doPasswordsMatch
-          ? <ErrorPasswordMatch ErrorInPasswordMatch={!this.state.doPasswordsMatch} />
-          : null
-        }
-        {
-          this.state.errorMsg !== "" 
-          ? this.displayErrorMessage()
-          : ""
-        }
-        <div className="form-group">
-          <button type="submit" className="btn btn-block" onClick={this.register}>Sign Up</button>
-        </div>
-      
-        </form>
+          <Field type="password" name="pswrdConfirmation" className="form-control" placeholder="Confirm Password" />
+
+          <button type="submit" className="btn btn-block">Sign Up</button>
+        </Form>
       </div>
     </div>
 
@@ -146,4 +69,22 @@ class Signup extends Component {
   }
 }
 
-export default Signup;
+const SignupFormik = withFormik({
+  mapPropsToValues({email, password}) {
+    return {
+      email: email || "",
+      password: password || ""
+    }
+  },
+  validationSchema: Yup.object().shape({
+    email: Yup.string().email().required(),
+    password: Yup.string().
+      min(MinPasswordLength, `Password must be at least ${MinPasswordLength} characters long`).
+      required("Must Enter password field")
+  }),
+  handleSubmit(values) {
+    console.log(values);
+  }
+})(Signup);
+
+export default SignupFormik;
